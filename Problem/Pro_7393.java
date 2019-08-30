0e00f98bb54df0924fb121465d247e413ad9bcb1
DESKTOP-F27IQ8Q\JEONG WOO JIN <wjddnwls918@naver.com>
2019-08-30 16:44:45 +0900

    #SWEA-Pro_7393

---
+++ Problem/Pro_7393.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Pro_7393 {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		
+	}
+
+}
