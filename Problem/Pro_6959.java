3648850aadb39a16109ae3a38d4f0467dd28f2a1
DESKTOP-F27IQ8Q\JEONG WOO JIN <wjddnwls918@naver.com>
2019-08-30 16:43:54 +0900

    #SWEA-Pro_6959

--- Problem/Pro_6959.java
+++ Problem/Pro_6959.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Random;
 
 public class Pro_6959 {
 
@@ -7,18 +8,46 @@ public class Pro_6959 {
 		// TODO Auto-generated method stub
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(in.readLine());
-		for(int x=1; x<=T; x++) {
-			
+		for (int x = 1; x <= T; x++) {
+
 			String num = in.readLine();
+
+			Random ran = new Random();
+
+			int turn = 0;
+			
+			while (num.length() != 1) {
+				String temp = "";
+				int pos = ran.nextInt(num.length() - 1);
+
+				temp += num.substring(0, pos);
+
+				int left, right;
+				left = Integer.parseInt(num.charAt(pos) + "");
+				right = Integer.parseInt(num.charAt(pos + 1) + "");
+
+				temp += Integer.toString(left+right);
+				
+				if (pos + 2 < num.length()) {
+					temp += num.substring(pos + 2, num.length());
+				}
+				
+				
+				
+				turn += 1;
+				turn %= 2;
+				
+				num = temp;
+
+			}
+
+			if(turn == 0) {
+				System.out.println("#" + x + " B");
+			} else {
+				System.out.println("#" + x + " A");
+			}
 			
 			
-			
-			
-			
-			
-			
-			
-			System.out.println("#"+x+" ");
 		}
 	}
 
